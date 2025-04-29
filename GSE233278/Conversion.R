e77421ed0b6a1e74916c1f62cbddb853ce1b7e89
aumchampaneri <aum.champaneri@outlook.com>
2025-04-29 16:13:17 -0400

    Add Conversion.R script for Seurat to h5ad format conversion and update .gitignore

---
+++ GSE233278/Conversion.R
@@ -0,0 +1,4 @@
+library(Seurat)
+library(SeuratDisk)
+
+Convert("/Users/aumchampaneri/PycharmProjects/Complement-OUD/GSE233279/GSE233278_OUD_Striatum_refined_all_SeuratObj_N16.h5Seurat", dest = "h5ad")
