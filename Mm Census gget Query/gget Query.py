b5853f227efd028ff045dc2ed6a81023a55d13ad
aumchampaneri <aum.champaneri@outlook.com>
2025-05-21 15:25:36 -0400

    Enhance data querying and processing scripts: update cell type filtering for glial and immune cells in gget Query.py, add KEGG and BioMart alignment functionality, and implement pathway gene mapping in KEGG Pathways.py. Create new output files for merged data and unique gene lists.

--- Mm Census gget Query/gget Query.py
+++ Mm Census gget Query/gget Query.py
@@ -1,4 +1,3 @@
-
 # Prepare the environment
 import gget
 import pandas as pd
@@ -6,89 +5,80 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scanpy as sc
-from numpy.lib.npyio import savez
 
 gget.setup("cellxgene")
 
-# Uncomment the following line to see the documentation
-# help(gget.cellxgene)
-
-'''
-Query the cellxgene database for the genes of interest
-Filters:
-disease: normal
-tissue_general: brain
-
-> Query Time: 3m 30s
-'''
-
+# Query the cellxgene database for glial and immune cells from normal mouse brain
+# This pulls only cells relevant to memory-constrained glia/immune-focused analyses
 adata = gget.cellxgene(
     ensembl=True,
     verbose=True,
     species='mus_musculus',
     disease='normal',
-    tissue_general='brain'
+    tissue_general='brain',
+    cell_type=[
+        # Glial cells
+        "astrocyte",
+        "oligodendrocyte",
+        "oligodendrocyte precursor cell",
+        "microglial cell",
+        "Bergmann glial cell",
+        "astrocyte of the cerebellum",
+        "glioblast",
+        "macroglial cell",
+        "ependymal cell",
+        "tanycyte",
+        "immature astrocyte",
+        "hypendymal cell",
+
+        # Immune cells
+        "macrophage",
+        "CD4-positive, alpha-beta T cell",
+        "CD8-positive, alpha-beta T cell",
+        "B cell",
+        "natural killer cell",
+        "monocyte",
+        "innate lymphoid cell",
+        "meningeal macrophage",
+        "perivascular macrophage",
+        "central nervous system macrophage",
+        "plasma cell",
+        "mature NK T cell",
+        "mast cell",
+        "conventional dendritic cell",
+        "plasmacytoid dendritic cell",
+        "T cell"
+    ]
 )
 
-all_genes = {
-"""
-Dictionary mapping a subset of gene names to their corresponding Ensembl IDs.
+# Optional: Add a minimal subset of neurons for reference or QC
+# Downsample neuron cell types post-query if needed
+if "cell_type" in adata.obs.columns:
+    neuron_like = adata[adata.obs["cell_type"].str.contains("neuron", case=False, na=False)]
+    neuron_sample = neuron_like[np.random.choice(neuron_like.shape[0], size=min(5000, neuron_like.shape[0]), replace=False)]
+    adata = adata.concatenate(neuron_sample, batch_key="subset", batch_categories=["glia_immune", "neurons_subsampled"])
 
-Keys:
-   str: Gene names (e.g., 'C3', 'C3AR1', etc.)
+# Save unprocessed raw data
+adata.write_h5ad("/Users/aumchampaneri/PycharmProjects/Complement-OUD/Mm Census gget Query/Data Files/Mouse_Census_Brain.h5ad")
 
-Values:
-   str: Ensembl IDs (e.g., 'ENSG00000125730', 'ENSG00000171860', etc.)
-"""
-    "C3": "ENSMUSG00000024164",
-    "C3AR1": "ENSMUSG00000040552",
-    "C5": "ENSMUSG00000026874",
-    "C5ar1": "ENSMUSG00000049130",
-    "C5ar2": "ENSMUSG00000074361",
-    "Cfh": "ENSMUSG00000026365",
-    "C1qa": "ENSMUSG00000036887",
-    "C1qb": "ENSMUSG00000036905",
-    "C1qc": "ENSMUSG00000036896",
-    "C4a": "ENSMUSG00000015451",
-    "C4b": "ENSMUSG00000073418"
-}
-
-
-"""
-Preprocess the kidney dataset by filtering genes, normalizing counts,
-log-transforming the data, and scaling the data. Then, perform PCA
-and plot an overview of the PCA results.
-
-Steps:
-1. Filter genes with at least 1 count.
-2. Normalize the total counts to 1e6.
-3. Log-transform the data.
-4. Scale the data.
-5. Perform PCA.
-6. Plot an overview of the PCA results.
-"""
-
-adata.write_h5ad(filename="/Users/aumchampaneri/PycharmProjects/Complement-OUD/Mm Census gget Query/Data Files/Mouse_Census_Brain.h5ad")
-
+# Basic filtering and preprocessing
 sc.pp.filter_genes(adata, min_counts=3)
 sc.pp.filter_cells(adata, min_genes=500)
 sc.pp.normalize_total(adata, target_sum=1e6)
 sc.pp.log1p(adata)
-# sc.pp.scale(adata) # Uses too much RAM -> swap to a zero center pca
 
-# Save the raw (normalized + log-transformed, but unscaled) data
+# Save normalized but unscaled version
 adata.raw = adata.copy()
 
-# Calculate PCA
+# PCA without zero-centering (to save RAM)
 sc.tl.pca(adata, zero_center=False)
 
-# Algorithically integrate multiple experiments
+# Harmony integration across datasets
 sc.external.pp.harmony_integrate(adata, key='dataset_id')
 
-# Calculate UMAP
+# Neighbors + UMAP
 sc.pp.neighbors(adata, use_rep='X_pca_harmony')
 sc.tl.umap(adata)
 
-adata.write_h5ad(
-    filename="/Users/aumchampaneri/PycharmProjects/Complement-OUD/Mm Census gget Query/Data Files/Mouse_Census_Brain_PP.h5ad",
-)
+# Save final processed object
+adata.write_h5ad("/Users/aumchampaneri/PycharmProjects/Complement-OUD/Mm Census gget Query/Data Files/Mouse_Census_Brain_PP.h5ad")
